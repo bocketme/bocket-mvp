8d247b0cca6cb112c64e98f5a94321ea101865b9
Mesquita Vincent <vincent.mesquita@epitech.eu>
2017-11-03 16:43:51 +0100

    :construction: UPDATE User sign up popup

--- public/js/logbox.js
+++ public/js/logbox.js
@@ -10,23 +10,28 @@ jQuery(function() {
     var hidden = "none";
     var visible = "flex";
 
+    var delay = 200;
+
     $("#closeBox").click(function() {
         console.log("close");
-        box.delay(250).fadeOut('slow');
+        box.delay(delay).fadeOut('slow');
     });
 
     launchButton.click(function() {
         console.log(box.css(visibility));
         if (box.css(visibility) === visible)
         {
-            box.delay(250).fadeOut('slow');
+            box.delay(delay).fadeOut('slow');
             console.log("if");
         }
         else
         {
             $("#userSignUp").get(0).reset();
             $("input").val('');
-            box.delay(250).fadeIn('slow');
+            box.css({
+                opacity: 0,
+                display: visible
+            }).animate({opacity:1}, delay + 250);
             console.log("else");
         }
     });
