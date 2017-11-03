5db4f4a2279cc95654d0c0699281900e7ead95e8
Mesquita Vincent <vincent.mesquita@epitech.eu>
2017-11-03 20:49:10 +0100

    :art: Code improving

--- public/js/logbox.js
+++ public/js/logbox.js
@@ -1,38 +1,60 @@
 jQuery(function() {
+    var visibility = "display";
+    var hidden = "none";
+    var visible = "flex";
+    var delay = 200;
+
 
     var lauchButtonId = "#signUp";
     var boxId = "#userSignUpBox";
 
-    var launchButton = $(lauchButtonId);
-    var box = $(boxId);
+    signUp(lauchButtonId, boxId);
 
-    var visibility = "display";
-    var hidden = "none";
-    var visible = "flex";
+    $("submit-btn").click(function () {
+        console.log("submit");
+    });
 
-    var delay = 200;
+    function signUp(lauchButtonId, boxId) {
+        var lauchButtonId = lauchButtonId;
+        var boxId = boxId;
 
-    $("#closeBox").click(function() {
-        console.log("close");
+        var launchButton = $(lauchButtonId);
+        var box = $(boxId);
+        var blur = $("#blur");
+
+        $("#closeBox").click(function() {
+            console.log("close");
+            hideBox(box);
+            blur.css("opacity", "0").css("visibility", "hidden");
+        });
+
+        launchButton.click(function() {
+            console.log(box.css(visibility));
+            if (box.css(visibility) === visible)
+            {
+                hideBox(box);
+                blur.css("opacity", "0").css("visibility", "hidden");
+            }
+            else
+            {
+                blur.css("opacity", "0.7").css("visibility", "visible");
+                $("#userSignUp").get(0).reset();
+                $("input").val('');
+                showBox(box);
+            }
+        });
+    }
+
+    function showBox(box) {
+        console.log("icioiiio");
+        box.css({
+            opacity: 0,
+            display: visible
+        }).animate({opacity:1}, delay + 250);
+    }
+
+    function hideBox(box) {
         box.delay(delay).fadeOut('slow');
-    });
+    }
 
-    launchButton.click(function() {
-        console.log(box.css(visibility));
-        if (box.css(visibility) === visible)
-        {
-            box.delay(delay).fadeOut('slow');
-            console.log("if");
-        }
-        else
-        {
-            $("#userSignUp").get(0).reset();
-            $("input").val('');
-            box.css({
-                opacity: 0,
-                display: visible
-            }).animate({opacity:1}, delay + 250);
-            console.log("else");
-        }
-    });
 });
