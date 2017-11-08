8d615c69b46e8f422e3d5dca15487c2b2d35f7f5
Mesquita Vincent <vincent.mesquita@epitech.eu>
2017-11-08 18:37:09 +0100

    :construction: comment some useless code in app.js

--- app.js
+++ app.js
@@ -44,11 +44,11 @@ app.use(bodyParser.json());
 
 
 // Display body request
-app.use(function (req, res) {
+/*app.use(function (req, res) {
     res.setHeader('Content-Type', 'text/plain');
     res.write('you posted:\n');
     res.end(JSON.stringify(req.body, null, 2))
-});
+});*/
 
 app.set("view engine", "twig");
 
