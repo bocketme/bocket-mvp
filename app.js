a0b6037788705a2a92805a17586d5c584dcd5ba2
Mesquita Vincent <vincent.mesquita@epitech.eu>
2017-11-08 15:48:23 +0100

    :construction: Sign up action saves user in db

--- app.js
+++ app.js
@@ -42,6 +42,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
+
+// Display body request
+/*app.use(function (req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.write('you posted:\n');
+    res.end(JSON.stringify(req.body, null, 2))
+});*/
+
 app.set("view engine", "twig");
 
 app.use("/", index);
