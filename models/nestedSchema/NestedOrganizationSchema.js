c4480b64c6e97d2d78a517149613695eefb57fcc
Mesquita Vincent <vincent.mesquita@epitech.eu>
2017-11-16 16:04:47 +0100

    :bug: check nameOrganisation

--- models/nestedSchema/NestedOrganizationSchema.js
+++ models/nestedSchema/NestedOrganizationSchema.js
@@ -1,9 +1,12 @@
 let mongoose = require("mongoose");
+let uniqueValidator = require('mongoose-unique-validator');
 
 let NestedOrganizationSchema = new mongoose.Schema({
     _id : {type : mongoose.SchemaTypes.ObjectId, required: true},
-    name : {type: String, required: true},
+    name: {type: String, required: true, index: { unique: true }},
     end : {type: Date}
 });
+
+NestedOrganizationSchema.plugin(uniqueValidator);
 
 module.exports = NestedOrganizationSchema;
