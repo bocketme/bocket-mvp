5bd8034998592c64adff182d7b1bae791d897ecb
Hervé <herve.kay@ynov.com>
2017-12-06 19:39:09 +0100

    :construction: config server

--- config/server.js
+++ config/server.js
@@ -7,8 +7,10 @@ module.exports = {
     protocol: "http",
     mongoDB: "mongodb://localhost/bocketmemvp",
     saltRounds: 10,
+    tpm: require('path').resolve("../tpm"),
     gitfiles: require('path').resolve("../bocket"),
     avatar: require('path').resolve("../avatar"),
     specfiles: require('path').resolve("../spec"),
-    secretSession: "kdjqskdjkqsjdsjqdklqsjdkjziooajdiazjdskjdqklsjdjaziodjsqjdlj"
+    secretSession: "kdjqskdjkqsjdsjqdklqsjdkjziooajdiazjdskjdqklsjdjaziodjsqjdlj",
+    secretKey: "Why you're always lying ? Why you're always lying ?",
 };
