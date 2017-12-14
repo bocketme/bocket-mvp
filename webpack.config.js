b4b3674c546e00c3139cc67af690167bf7a98d35
Hervé <herve.kay@ynov.com>
2017-12-14 10:42:08 +0100

    ADD WEBPACK DIRECTORY - IGNORE DIST FILES

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    entry: "./public/js/viewer_v2/init.js",
+    entry: "./webpack/viewer/init.js",
     watch: true,
     output: {
         path: path.resolve("public/js/dist"),
