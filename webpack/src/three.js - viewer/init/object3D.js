1425fad19f0cbdd41d3653921a58c15f71232290
Hervé <herve.kay@ynov.com>
2017-12-21 14:53:29 +0100

    :construction: webpack THREE.js Viewer

--- webpack/src/three.js - viewer/init/object3D.js
+++ webpack/src/three.js - viewer/init/object3D.js
@@ -16,12 +16,15 @@ var loadObjectFromJSON = function (jsonObj, colors) {
     geometry.computeVertexNormals();
 
     var geometry_2 = new THREE.BoxGeometry( 50, 50, 50);
+
+    var material = new THREE.MeshBasicMaterial( { color: colors } );
+    var mesh = new THREE.Mesh( geometry_2, material );
+
     console.log(geometry);
     console.log(geometry_2);
 
-    var material = new THREE.MeshBasicMaterial( { color: colors } );
-    var mesh = new THREE.Mesh( geometry, material );
     return mesh;
+
     /* var geometry = new THREE.BufferGeometry();
      geometry.addAttribute( 'position', new THREE.BufferAttribute( vertices, 3 ) );
      var material = new THREE.MeshBasicMaterial( { color: colors } );
