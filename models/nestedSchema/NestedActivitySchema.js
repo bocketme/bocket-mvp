46f03e1136390cdaa09cc069dfc6f93bd0fbf232
Mesquita Vincent <vincent.mesquita@epitech.eu>
2017-12-21 12:52:39 +0100

    :card_file_box: NestedActivitySchema updated

--- models/nestedSchema/NestedActivitySchema.js
+++ models/nestedSchema/NestedActivitySchema.js
@@ -3,6 +3,7 @@ const uniqueValidator = require('mongoose-unique-validator');
 const NestedFile = require("./NestedFile");
 
 let activity = new mongoose.Schema({
+    _id : {type : mongoose.SchemaTypes.ObjectId, default: mongoose.Types.ObjectId()},
     type: {type: String, required: true},
     content: { type: String, required: true },
     date: { type: Date, default: new Date() },
