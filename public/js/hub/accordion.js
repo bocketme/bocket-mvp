68c274eac114d1c83d27af3b7ab5f2289f873c51
Hervé <herve.kay@ynov.com>
2017-12-27 20:05:48 +0100

    :construction: viewer

--- public/js/hub/accordion.js
+++ public/js/hub/accordion.js
@@ -20,43 +20,41 @@ function loadNodeInformation(e) {
     var node_type = element.contents().filter("span.p-node").attr("data-node");
 
     if (!nodeId)
-    Materialize.toast('Error, The node selected has no ID', 2000);
+        Materialize.toast('Error, The node selected has no ID', 2000);
 
-        //CSS EFFECT
-        if (!element.hasClass("selected-accordion")) {
-            if (node_type === NodeTypeEnum.part)
-                socket.emit('getPart', nodeId);
-            else if (node_type === NodeTypeEnum.assembly)
-                socket.emit('getAssembly', nodeId);
-            else {
-                Materialize.toast("Vous cherchez le chragement d'un noeud sans type", 200);
-            }
+    //CSS EFFECT
+    if (!element.hasClass("selected-accordion")) {
+        if (node_type === NodeTypeEnum.part)
+            socket.emit('getPart', nodeId);
+        else if (node_type === NodeTypeEnum.assembly)
+            socket.emit('getAssembly', nodeId);
+        else {
+            Materialize.toast("Vous cherchez le chragement d'un noeud sans type", 200);
         }
-        $('.collapsible-header.three-node').removeClass('selected-accordion');
-        element.addClass('selected-accordion');
-
-        // Value to change - VUE.JS
-        third_column.selectNode(fill_value, breadcrumbs_value);
-
-        //Socket To Emit
-        socket.emit("nodeInformation", nodeId);
-        if ($(selector).hasClass("container")) {
-            console.log("socket Emitted");
-            socket.emit("nodeChildren", nodeId, breadcrumbs_value, sub_level);
-        }
-
-        clearComments($("#activity-comments-location"));
-        clearComments($("#activity-comments-content"));
-        socket.emit("getActivities", {
-            nodeId: idOfchoosenNode,
-            viewType: ViewTypeEnum.location
-        });
-        socket.emit("getActivities", {
-            nodeId: idOfchoosenNode,
-            viewType: ViewTypeEnum.content
-        });
-
     }
+    $('.collapsible-header.three-node').removeClass('selected-accordion');
+    element.addClass('selected-accordion');
+
+    // Value to change - VUE.JS
+    third_column.selectNode(fill_value, breadcrumbs_value);
+
+    //Socket To Emit
+    socket.emit("nodeInformation", nodeId);
+    if ($(selector).hasClass("container")) {
+        console.log("socket Emitted");
+        socket.emit("nodeChildren", nodeId, breadcrumbs_value, sub_level);
+    }
+
+    clearComments($("#activity-comments-location"));
+    clearComments($("#activity-comments-content"));
+    socket.emit("getActivities", {
+        nodeId: idOfchoosenNode,
+        viewType: ViewTypeEnum.location
+    });
+    socket.emit("getActivities", {
+        nodeId: idOfchoosenNode,
+        viewType: ViewTypeEnum.content
+    });
 }
 
 $(document).ready(() => {
