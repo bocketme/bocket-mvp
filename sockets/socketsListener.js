df9ed49bd096586f5acf61096e29b6cf7b4a809c
Mesquita Vincent <vincent.mesquita@epitech.eu>
2018-02-02 12:28:40 +0100

    :construction: DeleteNode, rights are checked

--- sockets/socketsListener.js
+++ sockets/socketsListener.js
@@ -1,17 +1,18 @@
-let betaRegistrationListener = require("./betaRegistrationSListener");
-let checkUniqueField = require("./checkUniqueField");
-let signinListener = require("./signinListener");
-let newNodeListener = require("./newNodeListener");
-let NodeInformationListener = require("./nodeInformationListener");
-let contentInformationListener = require('./contentInformationListener');
-let searchNodeChildren = require('./searchNodeChildren');
-let nodeViewer = require('./nodeViewer');
-let newActivityComment = require('./newActivityCommentListener');
-let getActivities = require("./getActivitiesListener");
-let addCommentListener = require("./addCommentToActivityListener");
-let joinWorkspaceListener = require("./joinWorkspaceListener");
-let leaveWorkspaceListener = require("./leaveWorkspaceListener");
-let invitePeopleListener = require("./invitePeopleListener");
+const betaRegistrationListener = require("./betaRegistrationSListener");
+const checkUniqueField = require("./checkUniqueField");
+const signinListener = require("./signinListener");
+const newNodeListener = require("./newNodeListener");
+const NodeInformationListener = require("./nodeInformationListener");
+const contentInformationListener = require('./contentInformationListener');
+const searchNodeChildren = require('./searchNodeChildren');
+const nodeViewer = require('./nodeViewer');
+const newActivityComment = require('./newActivityCommentListener');
+const getActivities = require("./getActivitiesListener");
+const addCommentListener = require("./addCommentToActivityListener");
+const joinWorkspaceListener = require("./joinWorkspaceListener");
+const leaveWorkspaceListener = require("./leaveWorkspaceListener");
+const invitePeopleListener = require("./invitePeopleListener");
+const deleteNodeListener = require("./deleteNodeListener");
 
 module.exports = function(io) {
     io.on('connection', function (socket) {
@@ -29,5 +30,6 @@ module.exports = function(io) {
         invitePeopleListener(socket);
         joinWorkspaceListener(io, socket);
         leaveWorkspaceListener(io, socket);
+        deleteNodeListener(socket);
     });
 };
