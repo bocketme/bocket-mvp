7d4c269477befeeee8bf52e8e7606090a03e1ec1
Mesquita Vincent <vincent.mesquita@epitech.eu>
2018-02-08 16:53:17 +0100

    :lipstick: [issue/26] file format in uppercase

--- public/js/hub/specs.js
+++ public/js/hub/specs.js
@@ -58,7 +58,7 @@ function addSpec(lastSpec, file) {
             "    <div class=\"col s12\">\n" +
             "        <div class=\"col s1\"><img src=\"/img/file-icon.svg\"></div>\n" +
             "        <span class=\"col s10 file-title\">" + file.name + "</span>\n" +
-            "        <span class=\"col s1 format\">" + file.format + "</span>\n" +
+            "        <span class=\"col s1 format\">" + file.format.toUpperCase() + "</span>\n" +
             "    </div>\n" +
             "</div>\n" +
         "</li>");
