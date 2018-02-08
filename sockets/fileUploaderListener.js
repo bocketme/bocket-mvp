b4a55a55fe220c2d3c12560f199bf7403137615e
Mesquita Vincent <vincent.mesquita@epitech.eu>
2018-02-08 16:41:39 +0100

    :construction: [issue/26] file correctly uploaded in the good directory

--- sockets/fileUploaderListener.js
+++ sockets/fileUploaderListener.js
@@ -1,5 +1,39 @@
 const mv = require('mv');
 const Node = require('../models/Node');
+const Workspace = require('../models/Workspace');
+const Part = require('../models/Part');
+const Assembly = require('../models/Assembly');
+const path = require('path');
+const NodeTypeEnum = require('../enum/NodeTypeEnum');
+
+const appDir = path.dirname(require.main.filename);
+
+/**
+ * get the right uploadDir
+ * @param fileName : { String }
+ * @param nodeId : { String }
+ * @param workspaceId : { String }
+ * @return {Promise<String>}
+ */
+async function getUploadir(fileName, nodeId, workspaceId) {
+  let ret = '';
+  let content = '';
+  try {
+    const node = await Node.findById(nodeId);
+    const workspace = await Workspace.findById(workspaceId);
+    if (!node || !workspace) throw Error('Unknown node');
+    if (node.type === NodeTypeEnum.part) {
+      content = await Part.findById(node.content);
+    } else if (node.type === NodeTypeEnum.part) {
+      content = await Assembly.findById(node.content);
+    }
+    if (!content) throw Error('Unknown content');
+    ret = `${appDir}/data/files3D/${workspace.organization.name}/${content.name} - ${node.content}/import/${fileName}`;
+  } catch (e) {
+    throw e;
+  }
+  return ret;
+}
 
 module.exports = (socket, uploader) => {
   uploader.on('start', (fileInfo) => {
@@ -10,8 +44,16 @@ module.exports = (socket, uploader) => {
     console.log(`${fileInfo.wrote} / ${fileInfo.size} byte(s)`);
   });
   uploader.on('complete', (fileInfo) => {
-    console.log('Upload Complete.');
-    console.log(fileInfo);
+    getUploadir(fileInfo.name, fileInfo.data.nodeId, socket.handshake.session.currentWorkspace)
+      .then((ret) => {
+        mv(`${appDir}/${fileInfo.uploadDir}`, ret, () => {});
+        fileInfo.uploadDir = ret;
+        console.log('Upload Complete.');
+        console.log(fileInfo);
+      })
+      .catch((err) => {
+        console.log('Error!', err);
+      });
   });
   uploader.on('error', (err) => {
     console.log('Error!', err);
@@ -20,17 +62,3 @@ module.exports = (socket, uploader) => {
     console.log('Aborted: ', fileInfo);
   });
 };
-
-/**
- * get the right uploadDir
- * @param nodeId : { String }
- * @return {Promise<String>}
- */
-/*async function getUploadir(nodeId) {
-  try {
-    await
-  } catch (e) {
-    throw e;
-  }
-  return '';
-}*/
