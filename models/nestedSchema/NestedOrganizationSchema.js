096bfdb3a08786a3be4d429129ca45fcb53f1a67
RePloZ <herve.kay@ynov.com>
2018-02-13 19:00:26 +0100

    [Suggestion] ? :fire: the end

--- models/nestedSchema/NestedOrganizationSchema.js
+++ models/nestedSchema/NestedOrganizationSchema.js
@@ -3,7 +3,8 @@ let mongoose = require("mongoose");
 let NestedOrganizationSchema = new mongoose.Schema({
     _id : {type : mongoose.SchemaTypes.ObjectId, required: true},
     name: {type: String, required: true},
-    end : {type: Date}
+    //TODO: Delete the end Date.
+    //end : {type: Date}
 });
 
 module.exports = NestedOrganizationSchema;
