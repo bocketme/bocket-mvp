51c91a8b2face58100513017f69d1b610a16255c
Jean-Adrien Domage <jeanadrien.domage@epitech.eu>
2018-02-23 18:52:20 +0100

    :construction::lipstick: update UI for invitation process

--- public/js/hub.js
+++ public/js/hub.js
@@ -46,6 +46,21 @@
             $('#pref-option').css('top', (parseInt(top) + 20) + "px");
         });
 
+        $('.team-option-style').dropdown({
+            inDuration: 300,
+            outDuration: 225,
+            constrainWidth: false, // Does not change width of dropdown to that of the activator
+            hover: false, // Activate on hover
+            gutter: 0, // Spacing from edge
+            belowOrigin: true, // Displays dropdown below the button
+            alignment: 'down', // Displays dropdown with edge aligned to the left of button
+            stopPropagation: false // Stops event propagation
+        });
+        $('.team-option-style').on('click', () => {
+            let top = $('#team-option').css('top');
+            $('#team-option').css('top', (parseInt(top) + 20) + "px");
+        });
+
         $(".triple-dots").dropdown({
             inDuration: 300,
             outDuration: 225,
