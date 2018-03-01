325d9049f692c6260dfe9ca68d91a8c55c618b37
RePloZ <herve.kay@ynov.com>
2018-03-01 14:40:14 +0100

    webpack + viewer change dest files

--- dev.config.js
+++ dev.config.js
@@ -1,6 +1,6 @@
 const merge = require('webpack-merge');
 const baseConfig = require('./webpack.config');
-
+const webpack = require('webpack');
 module.exports = merge (baseConfig, {
   devtool: 'eval-source-map',
   watch: true,
