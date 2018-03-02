627bc0c10caf96789aa46f4313ce57f9a496ed39
Jean-Adrien Domage <jeanadrien.domage@epitech.eu>
2018-03-02 14:58:28 +0100

    :construction: socket emit for organisation owenr/members

--- sockets/workspaceListener.js
+++ sockets/workspaceListener.js
@@ -1,20 +1,42 @@
 const workspaceListenerName = 'workspaceManager';
 const WorkspaceModel = require('../models/Workspace');
+const OrganizationModel = require('../models/Organization');
+const log = require('../utils/log');
 
 async function workspaceListener(workspaceId) {
   const { team } = await WorkspaceModel.findById(workspaceId);
-  return { owner: false, members: team.members };
+  return team;
+}
+
+async function organizationListener(workspaceId) {
+    const { organization } = await WorkspaceModel.findById(workspaceId);
+    const { owner, members } = await OrganizationModel.findById(organization._id);
+    return { owner, members };
 }
 
 module.exports = (socket) => {
   socket.on(workspaceListenerName, ({ type }) => {
-    if (type === 'workspace') {
-      workspaceListener(socket.handshake.session.currentWorkspace)
-        .then((data) => {
-          console.log('data:', data);
-          socket.emit(workspaceListenerName, data);
-        })
-        .catch(console.log);
-    }
+      switch (type) {
+
+          case 'worspace':
+              workspaceListener(socket.handshake.session.currentWorkspace)
+                  .then(data => {
+                      console.log('data:', data);
+                      socket.emit(workspaceListenerName, data);
+                  })
+                  .catch(log.error);
+              break;
+
+          case 'organization':
+              organizationListener(socket.handshake.session.currentWorkspace)
+                  .then(data => {
+                      socket.emit(workspaceListenerName, data);
+                  })
+                  .catch(log.error);
+              break;
+
+          default:
+            socket.emit(workspaceListenerName, null);
+      }
   });
 };
