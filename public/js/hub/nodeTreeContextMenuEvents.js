b8d0242a0819737b6fc0ffcf785ef2731c10b686
RePloZ <herve.kay@ynov.com>
2018-03-08 19:25:16 +0100

    :wrench: Front socket delete node

--- public/js/hub/nodeTreeContextMenuEvents.js
+++ public/js/hub/nodeTreeContextMenuEvents.js
@@ -1,11 +1,13 @@
 var p = workspaceId;
 
 $(document).ready(function () {
-        const contextMenu = $("#node-tree-context-menu");
+    const contextMenu = $("#node-tree-context-menu");
 
-        contextMenu.on("click", "#remove", function () {
-            var node = $(pointedElem);
-            console.log("deletede : ", node.attr("id"));
-            socket.emit("deleteNode", node.attr("id"));
-        });
+    contextMenu.on("click", "#remove", function () {
+        var node = $(pointedElem);
+        if (!node.attr('id'))
+            node = node.parent('div.three-node');
+        console.log("deletede : ", node.attr("id"));
+        socket.emit("deleteNode", node.attr("id"));
+    });
 });
