9880ee9cf3a8bc70cb7f610d1e2a6f781faa0697
RePloZ <herve.kay@ynov.com>
2018-03-08 17:03:50 +0100

    :speech_balloon: log pino

--- sockets/searchNodeChildren.js
+++ sockets/searchNodeChildren.js
@@ -6,22 +6,19 @@ module.exports = (socket) => {
     socket.on("nodeChildren", (nodeId, breadcrumbs, sub_level) => {
         Node.findById(nodeId)
             .then((node) => {
-                console.log(node.name);
-                console.log(node.children);
                 node.children.forEach(child => {
                     if(child.title)
                         child.name = child.title;
                         child.breadcrumb = breadcrumbs + '/' + child.name;
                     });
                 sub_level++;
-                console.log(sub_level);
                 twig.renderFile('./views/socket/three_child.twig', {
                     node: node,
                     TypeEnum: TypeEnum,
                     sub_level: sub_level
                 }, (err, html) => {
                     if (err){
-                        console.log(err);
+                        log.error('[Socket] - node Children\n', err);
                         socket.emit("error", err);
                     }
                     else socket.emit("nodeChild", html, nodeId);
