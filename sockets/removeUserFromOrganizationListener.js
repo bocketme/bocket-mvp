0415ffd6e8477a32a7cb55b88208519ee034a402
Jean-Adrien Domage <jeanadrien.domage@epitech.eu>
2018-03-08 15:12:50 +0100

    :construction: add security check on manageWorkspace

--- sockets/removeUserFromOrganizationListener.js
+++ sockets/removeUserFromOrganizationListener.js
@@ -10,18 +10,23 @@ async function removeInWorkspace(organization, userEmail) {
 }
 
 async function removeUserFromOrganization(workspaceId, userEmail) {
-    const { organization } = await WorkspaceModel.findById(workspaceId);
-    const userOrganization = await OrganizationModel.findById(organization.id);
-    removeInWorkspace(userOrganization, userEmail);
+    const { owner, organization } = await WorkspaceModel.findById(workspaceId);
+    const workspaceOrganization = await OrganizationModel.findById(organization.id);
+    if (userEmail === owner.email)
+        return false;
+    removeInWorkspace(workspaceOrganization, userEmail)
+        .catch((err) => {
+            throw err;
+        });
     await OrganizationModel.update({ _id : organization.id }, { $pull : { 'members' : { email : userEmail }}});
-    return userOrganization;
+    return true;
 }
 
 module.exports = (socket) => {
     socket.on(removeUserFromOrganizationName, ({ userEmail }) => {
         removeUserFromOrganization(socket.handshake.session.currentWorkspace, userEmail)
-            .then((workspace) => {
-                socket.emit(removeUserFromOrganizationName, true);
+            .then((state) => {
+                socket.emit(removeUserFromOrganizationName, state);
             })
             .catch((err) => {
                 internalErrorEmitter(socket);
--- sockets/removeUserFromWorkspaceListener.js
+++ sockets/removeUserFromWorkspaceListener.js
@@ -3,16 +3,18 @@ const WorkspaceModel = require('../models/Workspace');
 const internalErrorEmitter = require('./emitter/internalErrorEmitter');
 
 async function removeUserFromWorkspace(workspaceId, userEmail) {
-    const workspace = await WorkspaceModel.findById(workspaceId);
+    const { owner } = await WorkspaceModel.findById(workspaceId);
+    if (userEmail === owner.email)
+        return false;
     await WorkspaceModel.update({ _id : workspaceId }, { $pull : { 'team.members' : { email : userEmail }}});
-    return workspace;
+    return true;
 }
 
 module.exports = (socket) => {
     socket.on(removeUserFromWorkspaceName, ({ userEmail }) => {
         removeUserFromWorkspace(socket.handshake.session.currentWorkspace, userEmail)
-            .then((workspace) => {
-                socket.emit(removeUserFromWorkspaceName, true);
+            .then((state) => {
+                socket.emit(removeUserFromWorkspaceName, state);
             })
             .catch((err) => {
                 internalErrorEmitter(socket);
