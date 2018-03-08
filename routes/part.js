8163912d7dbc12336c3072d430f6c12a9cec78ff
RePloZ <herve.kay@ynov.com>
2018-03-08 13:48:18 +0100

    delete update route

--- routes/part.js
+++ routes/part.js
@@ -3,9 +3,7 @@ let router = express.Router();
 let upload = require('multer')();
 let postController = require("../controllers/Part/post");
 let getController = require("../controllers/Part/get");
-const updateController = require('../controllers/Part/update');
 
-router.post('/update', updateController);
 router.post('/:nodeId', upload.fields([{name:'file3D', maxCount: 1}, {name:'specFiles'}]), postController.newPart);
 router.get('/:nodeId/modeler', getController.modeler);
 module.exports = router;
