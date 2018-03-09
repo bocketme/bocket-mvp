d33f9059be098eb049c7767c564f4dec0081c692
Mesquita Vincent <vincent.mesquita@epitech.eu>
2018-03-09 22:20:12 +0100

    :construction: reportIssueListener

--- sockets/reportIssueListener.js
+++ sockets/reportIssueListener.js
@@ -13,11 +13,12 @@ module.exports = (socket) => {
       subject: 'ISSUE BOCKET.ME',
       html: `Issue créée par ${socket.handshake.userMail}<br>TITLE: ${title}<br><br>DESCRIPTION: ${description}`,
     };
-    mailTransporter.sendMail(mailOptions, (error) => {
+    mailTransporter.sendMail(mailOptions, (error, info) => {
       if (error) {
-        console.log('error while sending mail:', error);
+        console.log('[ReportIssue] error:', info);
         actionFailed(socket, { title: 'Report Issue', description: 'An error occured, please try again' });
       } else {
+        console.log('[ReportIssue] success:', info);
         actionSucceeded(socket, { title: 'Report Issue', description: 'The issue has been reported' });
       }
     });
