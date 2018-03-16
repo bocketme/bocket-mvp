e5c7163c58ce3492a4acccf4a64d2dbd79f97e2a
RePloZ <herve.kay@ynov.com>
2018-03-16 15:21:40 +0100

    :construction: SignIn only in invitation

--- controllers/signUpController.js
+++ controllers/signUpController.js
@@ -1,6 +1,7 @@
 const escape = require('escape-html');
 const ModelsMaker = require('../models/utils/create/ModelsMaker');
 const nodeMasterConfig = require('../config/nodeMaster');
+const log = require('../utils/log');
 const NodeSchema = require('../models/Node');
 const Invitation = require('../models/Invitation');
 const Workspace = require('../models/Workspace');
@@ -12,6 +13,7 @@ const WorkspaceSchema = require('../models/Workspace');
 const acceptInvitation = require('../utils/Invitations/acceptInvitation');
 const signInUserSession = require('../utils/signInUserSession');
 const TeamSchema = require('../models/Team');
+const asyncForEach = require('../utils/asyncForeach');
 
 const signUpController = {
 
@@ -31,201 +33,12 @@ const signUpController = {
         return res.redirect('/');
       }
     }
-    const Documents = {};
-
-    const user = UserSchema.newDocument({
-      completeName: req.body.completeName,
-      password: req.body.password,
-      email: req.body.email,
-      workspaces: [],
-      organizations: [],
-    });
-
-    user.save()
-      .then((newUser) => {
-        console.log(Documents.user);
-        Documents.user = newUser;
-        const organization = OrganizationSchema.newDocument({
-          owner: {
-            _id: Documents.user._id,
-            completeName: Documents.user.completeName,
-            email: Documents.user.email,
-          },
-          members: [{
-            _id: Documents.user._id,
-            completeName: Documents.user.completeName,
-            email: Documents.user.email,
-          }],
-          name: req.body.organizationName,
-        });
-        return organization.save();
-      })
-      .then((newOrga) => {
-        console.log('new Organization is created', newOrga);
-        Documents.organization = newOrga;
-        console.log('Orga a ecrire dans user', `${Documents.organization._id}-${Documents.organization.name}`);
-        Documents.user.organizations.push({
-          _id: Documents.organization._id,
-          name: Documents.organization.name
-        });
-        return Documents.user.save();
-      })
-      .then((UserUpdate) => {
-        Documents.user = UserUpdate;
-        console.log('user courant :', Documents.user);
-        const team = TeamSchema.newDocument({
-          owners: [{
-            _id: Documents.user._id,
-            completeName: Documents.user.completeName,
-            email: Documents.user.email,
-          }],
-          members: [{
-            _id: Documents.user._id,
-            completeName: Documents.user.completeName,
-            email: Documents.user.email,
-          }],
-          //   consults: [{}],
-        });
-        return team.save();
-      })
-
-      .then((newTeam) => {
-        console.log('\n\nnew team has been add', newTeam);
-        Documents.team = newTeam;
-        const workspace = WorkspaceSchema.newDocument({
-          name: req.body.workspaceName,
-          owner: {
-            _id: Documents.user._id,
-            completeName: Documents.user.completeName,
-            email: Documents.user.email,
-          },
-          organization: {
-            _id: Documents.organization._id,
-            name: Documents.organization.name,
-          },
-          team: {
-            _id: Documents.team._id,
-            owners: Documents.team.owners,
-            members: Documents.team.members,
-            consults: Documents.team.consults,
-          },
-        });
-
-
-        if (req.body.invitationUid) {
-          console.log('INVITATION');
-          acceptInvitation(req.body.invitationUid, Documents.user)
-            .then((invitationInfo) => {
-              req.session = signInUserSession(req.session, {
-                email: user.email
-              });
-              req.session.completeName = Documents.user.completeName;
-              req.session.currentWorkspace = invitationInfo.workspaceId;
-              res.redirect(`/project/${invitationInfo.workspaceId}`);
-            })
-            .catch((err) => {
-              console.log(err);
-              newOrga.remove();
-              newUser.remove();
-            });
-          return Promise.reject('Invitation');
-        }
-        return workspace.save();
-      })
-      .then((newWorkspace) => {
-        console.log('\n\nnew workspace has been add \n', newWorkspace);
-        Documents.workspace = newWorkspace;
-        Documents.user.workspaces.push({
-          _id: Documents.workspace._id,
-          name: Documents.workspace.name
-        });
-        // TODO: Documents.user.organizations
-        return Documents.user.save();
-      })
-      .then(() => {
-        const assembly = AssemblySchema.newDocument({
-          name: req.body.workspaceName,
-
-          description: nodeMasterConfig.description,
-          ownerOrganization: {
-            _id: Documents.organization._id,
-            name: Documents.organization.name,
-          },
-          creator: {
-            _id: Documents.user._id,
-            completeName: Documents.user.completeName,
-            email: Documents.user.email,
-          },
-        });
-        return assembly.save();
-      })
-      .then((newAssembly) => {
-        Documents.assembly = newAssembly;
-        console.log('\n\nnew assembly has been add \n', Documents.assembly);
-        const node = NodeSchema.newDocument({
-          name: req.body.workspaceName,
-          description: nodeMasterConfig.description,
-          type: NodeTypeEnum.assembly,
-          content: Documents.assembly._id,
-          Users: [{
-            _id: Documents.user._id,
-            completeName: Documents.user.completeName,
-            email: Documents.user.email,
-          }],
-          owners: [{
-            _id: Documents.user._id,
-            completeName: Documents.user.completeName,
-            email: Documents.user.email,
-          }],
-          team: {
-            _id: Documents.team._id,
-            owners: Documents.team.owners,
-            members: Documents.team.members,
-            consults: Documents.team.consults,
-          },
-          Workspaces: [{
-            "_id": Documents.workspace._id,
-            "name": Documents.workspace.name
-          }],
-        });
-        return node.save();
-      })
-      .then((nodeMaster) => {
-        console.log('\n\nnew node has been add \n', nodeMaster);
-        Documents.node = nodeMaster;
-        Documents.workspace.node_master = nodeMaster;
-        return Documents.workspace.save();
-      })
-      .then(() => {
-        Documents.organization.workspaces.push({
-          _id: Documents.workspace._id,
-          name: Documents.workspace.name,
-        });
-        return Documents.organization.save();
-      })
-      .then(() => {
-        Documents.assembly.whereUsed.push(Documents.node._id);
-        return Documents.assembly.save();
-      })
-      .then(() => {
-        req.session = signInUserSession(req.session, {
-          email: Documents.user.email
-        });
-        req.session.completeName = Documents.user.completeName;
-        req.session.currentWorkspace = Documents.workspace._id;
-        res.redirect(`/project/${Documents.workspace._id}`);
-      })
+    createAccount(req, res)
       .catch((err) => {
-        if (err === 'Invitation') return;
-        console.error(new Error(`[Sign up Controller] -  erreur \n${err}`));
-        Object.values(Documents).forEach((Documents) => {
-          if (Documents) {
-            Documents.remove();
-          }
-        });
-        res.status(400).send('Bad request');
+        log.error(err)
+        res.status(404).send('Bad Request');
       });
-  },
+  }
 };
 
 const passwordInfo = {
@@ -255,8 +68,7 @@ function checkOrganizationName(body) {
 
 function checkCompleteName(body) {
   const regex = /[A-Za-z/-]+ [A-Za-z/-]+/;
-  const completeName = body.completeName;
-  console.log('COMPLETENAME =', completeName);
+  const { completeName } = body;
   body.completeName = escape(body.completeName);
   return basicCheck(completeName, completeNameInfo) && regex.test(completeName);
 }
@@ -279,6 +91,143 @@ function checkEmail(body) {
 
 function basicCheck(parameter, parameterInfo) {
   return parameter !== undefined && parameter !== '' && parameter.length >= parameterInfo.minlength;
+}
+
+async function errorIncreationAccount(err, documentsToDelete) {
+  log.error(err);
+  asyncForEach(documentsToDelete, async (document) => {
+    await document.remove().catch((error) => { log.fatal('Document not Existing \n', error); });
+  });
+}
+async function createAccount(req, res) {
+  const documents = [];
+  try {
+    const user = await UserSchema.create({
+      completeName: req.body.completeName,
+      password: req.body.password,
+      email: req.body.email,
+      workspaces: [],
+      organizations: [],
+    });
+
+    await user.save();
+    documents.push(user);
+
+    const nestedUser = {
+      _id: user._id,
+      completeName: user.completeName,
+      email: user.email,
+    };
+
+    const organization = await OrganizationSchema.create({
+      owner: nestedUser,
+      members: [nestedUser],
+      name: req.body.organizationName,
+    });
+
+    await organization.save();
+    documents.push(organization);
+
+    const team = await TeamSchema.create({
+      owners: [nestedUser],
+      members: [nestedUser],
+    });
+
+    await team.save();
+    documents.push(team);
+
+    const workspace = await WorkspaceSchema.create({
+      name: req.body.workspaceName,
+      owner: nestedUser,
+      organization: {
+        _id: organization._id,
+        name: organization.name,
+      },
+      team: {
+        _id: team._id,
+        owners: team.owners,
+        members: team.members,
+        consults: team.consults,
+      },
+    });
+
+    await workspace.save();
+    documents.push();
+
+    if (req.body.invitationUid) {
+      const invitationInfo = await acceptInvitation(req.body.invitationUid, user);
+      req.session = signInUserSession(req.session, {
+        email: user.email,
+      });
+      req.session.completeName = user.completeName;
+      req.session.currentWorkspace = invitationInfo.workspaceId;
+      return res.redirect(`/project/${invitationInfo.workspaceId}`);
+    }
+    // Normal Signin desactivated.
+    throw new Error('Sign In - Desactivated');
+    /*
+    user.workspaces.push({
+      _id: workspace._id,
+      name: workspace.name,
+    });
+
+    await user.save();
+
+    const assembly = await AssemblySchema.create({
+      name: req.body.workspaceName,
+      description: nodeMasterConfig.description,
+      ownerOrganization: {
+        _id: organization._id,
+        name: organization.name,
+      },
+      creator: nestedUser,
+    });
+
+    await assembly.save();
+    documents.push(assembly);
+
+    const node = await NodeSchema.create({
+      name: req.body.workspaceName,
+      description: nodeMasterConfig.description,
+      type: NodeTypeEnum.assembly,
+      content: assembly._id,
+      Users: [nestedUser],
+      owners: [nestedUser],
+      team: {
+        _id: team._id,
+        owners: team.owners,
+        members: team.members,
+        consults: team.consults,
+      },
+      Workspaces: [{
+        _id: workspace._id,
+        name: workspace.name,
+      }],
+    });
+
+    await node.save();
+    documents.push(node);
+
+    workspace.node_master = node;
+    await workspace.save();
+
+    organization.workspaces.push({
+      _id: workspace._id,
+      name: workspace.name,
+    });
+    await organization.save();
+
+    assembly.whereUsed.push(node._id);
+    await assembly.save();
+
+    req.session = signInUserSession(req.session, { email: user.email });
+    req.session.completeName = user.completeName;
+    req.session.currentWorkspace = workspace._id;
+    return res.redirect(`/project/${workspace._id}`);
+    */
+  } catch (err) {
+    errorIncreationAccount(err, documents);
+  }
 }
 
 module.exports = signUpController;
