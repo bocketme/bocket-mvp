900733c2c244cba058c073995e3b67b3a1028fee
RePloZ <herve.kay@ynov.com>
2018-03-19 15:00:43 +0100

    :building_construction: config from .webpack.config.js is now a variable

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,31 @@
 const path = require('path');
-const webpack = require('webpack');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
-module.exports = {
-    entry: {
-        threejs:"./viewer/src/three.js - viewer/main.js",
-        webgl: "./viewer/src/webgl - viewer/main.js",
-        babylon:"./viewer/src/babylon.js - viewer/main.js",
-    },
-    watch: false,
-    devtool: "eval-source-map",
-    target: "web",
-    output: {
-        filename: '[name].js',
-        path: path.resolve("./public/js/viewer/bocket"),
-    },
-    plugins: [
-        new CleanWebpackPlugin(
-            ['bocket'], {root: path.resolve("./public/js/viewer/")}
-        ),
-    ]
+const config = {
+  entry: {
+    threejs: './viewer/src/three.js - viewer/main.js',
+    webgl: './viewer/src/webgl - viewer/main.js',
+    babylon: './viewer/src/babylon.js - viewer/main.js',
+  },
+  watch: false,
+  devtool: 'eval-source-map',
+  target: 'web',
+  output: {
+    filename: '[name].js',
+    path: path.resolve('./public/js/viewer/bocket'),
+  },
+  module: {
+    /*
+    rules: [{
+      test: /\.js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: ['babel-loader'],
+    }],
+    */
+  },
+  plugins: [
+    new CleanWebpackPlugin(['bocket'], { root: path.resolve('./public/js/viewer/') }),
+  ],
 };
+
+module.exports = config;
