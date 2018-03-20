4e19ccbece27a91d3556c8e5e568c705a06d5e7f
RePloZ <herve.kay@ynov.com>
2018-03-20 15:22:42 +0100

    add info workspace.id + rename workspace and organization

--- sockets/changeWorkspaceorOrganizationName.js
+++ sockets/changeWorkspaceorOrganizationName.js
@@ -2,6 +2,8 @@ const listenerName = 'changeWorkspaceOrOrganizationName';
 const User = require('../models/User');
 const Organization = require('../modals/Organization');
 const Workspace = require('../modals/Workspace');
+const twig = require('twig');
+const log = require('../utils/log');
 
 async function changeInformation(session, { type, id = null, newName }) {
   const user = await User.findOne({ email: session.userMail });
@@ -11,32 +13,24 @@ async function changeInformation(session, { type, id = null, newName }) {
   let dataToCHange;
   switch(type) {
     case 'workspace':
-    const dataToCHange = await Organization.findOne({'workspaces._id': session.currentWorkspace});
-    dataToCHange.name = newName;
-
-    await dataToCHange.save();
-    break;
+      const dataToCHange = await Organization.findOne({'workspaces._id': session.currentWorkspace});
+      dataToCHange.name = newName;
+      socket.emit('workspaceModifier', dataToCHange.name);
+      socket.emit('info', 'The workspace has been renamed');
+      await dataToCHange.save();
+      break;
 
     case 'organization':
-    const dataToCHange = await Workspace.findById(session.currentWorkspace);
-    dataToCHange.name = newName;
-    
-    await dataToCHange.save();
-    break;
-    
-    default :
-    return null
-    break;
+      const dataToCHange = await Workspace.findById(session.currentWorkspace);
+      dataToCHange.name = newName;
+      await dataToCHange.save();
+      socket.emit('organizationModifier', dataToCHange.name);
+      socket.emit('info', 'The organization has been renamed');
+      break;
   }
-  if (type == 'workspace'){
-
-  } else if(type == '') {
-
-  }
-
-
-  return { };
 }
+
+
 
 module.exports = (socket) => {
   socket.on(listenerName, (data) => {
@@ -44,7 +38,7 @@ module.exports = (socket) => {
       .then(result => socket.emit(listenerName, result))
       .catch((err) => {
         socket.emit(listenerName, { error: 'Internal Server Error, please try again' });
-        console.log('changePassword error:', err);
+        log.error('changePassword error:', err);
       });
   });
 };
