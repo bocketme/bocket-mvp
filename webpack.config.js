fa8e50595f488e55b6873127a5fc4ceb0ec192db
RePloZ <herve.kay@ynov.com>
2018-03-21 17:01:41 +0100

    :construction: webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,30 @@
 const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const UglifyJs = require('uglifyjs-webpack-plugin');
+const dev = process.env.NODE_ENV === "dev";
 
 const config = {
   entry: {
     threejs: './viewer/src/three.js - viewer/main.js',
-    webgl: './viewer/src/webgl - viewer/main.js',
-    babylon: './viewer/src/babylon.js - viewer/main.js',
+    //webgl: './viewer/src/webgl - viewer/main.js',
+    //babylon: './viewer/src/babylon.js - viewer/main.js',
   },
-  watch: false,
-  devtool: 'eval-source-map',
+  watch: dev,
+  devtool: dev ? 'cheap-module-eval-source-map' : false,
   target: 'web',
   output: {
     filename: '[name].js',
     path: path.resolve('./public/js/viewer/bocket'),
   },
-  module: {
-    /*
-    rules: [{
-      test: /\.js$/,
-      exclude: /(node_modules|bower_components)/,
-      use: ['babel-loader'],
-    }],
-    */
-  },
   plugins: [
     new CleanWebpackPlugin(['bocket'], { root: path.resolve('./public/js/viewer/') }),
   ],
 };
+
+if (!dev) {
+  config.plugins.push(new UglifyJs({
+    sourceMap: true
+  }));
+}
 
 module.exports = config;
