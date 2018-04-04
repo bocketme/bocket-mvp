746b00fc3a4d2943263664f873c964df464199ab
RePloZ <herve.kay@ynov.com>
2018-04-04 10:03:14 +0200

    add typescript conf

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,59 @@
 const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const UglifyJs = require('uglifyjs-webpack-plugin');
-const dev = process.env.NODE_ENV === "dev";
+const webpack = require('webpack')
+const tsImportPluginFactory = require('ts-import-plugin')
+
+const NODE_ENV = JSON.stringify(process.env.NODE_ENV)
 
 const config = {
   entry: {
+    app: './bocket-view/src/App.tsx',
     threejs: './viewer/src/three.js - viewer/main.js',
     //webgl: './viewer/src/webgl - viewer/main.js',
     //babylon: './viewer/src/babylon.js - viewer/main.js',
   },
-  watch: dev,
-  devtool: dev ? 'cheap-module-eval-source-map' : false,
+  devtool: NODE_ENV === 'development' ? 'cheap-module-eval-source-map' : false,
   target: 'web',
+  mode: 'none',
   output: {
     filename: '[name].js',
     path: path.resolve('./public/js/viewer/bocket'),
   },
+  module: {
+    rules: [{
+      test: /\.tsx?/,
+      loader: 'tslint-loader',
+      enforce: 'pre',
+      exclude: [/node_modules/]
+    },
+    {
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader'],
+    },
+    {
+      test: /\.tsx?/,
+      loader: 'ts-loader',
+      options: {
+        transpileOnly: true,
+        compilerOptions: {
+          module: 'es2015',
+        }
+      },
+      exclude: [/node_modules/]
+    },
+    {
+      test: /\.ts?/,
+      loader: 'ts-loader',
+      exclude: [/node_modules/]
+    },]
+  },
   plugins: [
+    new webpack.DefinePlugin({
+      'process.env': { NODE_ENV }
+    }),
     new CleanWebpackPlugin(['bocket'], { root: path.resolve('./public/js/viewer/') }),
   ],
 };
-
-if (!dev) {
-  config.plugins.push(new UglifyJs({
-    sourceMap: true
-  }));
-}
 
 module.exports = config;
