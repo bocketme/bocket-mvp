9f3e9e2a924fd2631e2edda00d5a3be38a91f167
RePloZ <herve.kay@ynov.com>
2018-04-12 10:59:56 +0200

    loading 3D - chargement Node

---
+++ controllers/Node/get.js
@@ -0,0 +1,92 @@
+const nodeSchema = require('../../models/Node');
+const nodeTypeEnum = require('../../enum/NodeTypeEnum')
+const partSchema = require('../../models/Part');
+const workspaceSchema = require('../../models/Workspace');
+const fs = require('fs');
+const path = require('path');
+const config = require('../../config/server');
+const PartFileSystem = require('../../config/PartFileSystem');
+const log = require('../../utils/log');
+const util = require('util')
+const mongoose = require('mongoose')
+const promisifyReadFile = util.promisify(fs.readFile)
+const promisifyReaddir = util.promisify(fs.readdir)
+const streamOptions = {
+  flags: 'r',
+  encoding: null,
+  mode: 0o666,
+  autoClose: true,
+}
+
+async function getFile3D(nodeId, workspaceId) {
+  const node = await nodeSchema.findById(nodeId)
+    .catch(err => {
+      throw (err)
+    })
+  if (!node) return null;
+  const part = await partSchema.findById(node.content)
+    .catch(err => {
+      throw (err)
+    });
+  if (!part) return null;
+
+  const file3Ddirectory = path.join(config.files3D, part.path, PartFileSystem.data)
+
+  let files = await promisifyReaddir(file3Ddirectory)
+    .catch(err => {
+      throw (err)
+    });
+console.log(files)
+  for (let i = 0; i < files.length; i++) {
+    if (path.extname(files[i]) == '.json') {
+      return path.join(file3Ddirectory, files[i])
+      break;
+    }
+  }
+  return null
+}
+
+async function getTextureFile(nodeId, workspaceId, textureName) {
+  const node = await nodeSchema.find({
+      '_id': mongoose.Types.ObjectId(nodeId),
+      'Workspaces._id': workspaceId
+    })
+    .catch(err => {
+      throw (err)
+    })
+  if (!node) return null;
+
+  const part = await partSchema.findById(node.content)
+    .catch(err => {
+      throw (err)
+    });
+  if (!part) return null;
+
+  const file3Ddirectory = path.join(config.files3D, part.path, PartFileSystem.data, path.basename(textureName))
+}
+
+module.exports = {
+  getFile3D: (req, res) => {
+    const {
+      nodeId
+    } = req.params
+    const workspaceId = req.session.currentWorkspace;
+    Promise.resolve(getFile3D(nodeId, workspaceId))
+      .then((file3D) => {
+        if (file3D) res.sendFile(file3D)
+        else res.status(403).send('Bad Request')
+      })
+  },
+  getFileTexture: (req, res) => {
+    const {
+      nodeId,
+      texture
+    } = req.params
+    const workspaceId = req.session.currentWorkspace;
+    Promise.resolve(getFile3D(nodeId, workspaceId, texture))
+      .then((textureFile) => {
+        if (textureFile) res.sendFile(textureFile)
+        else res.status(403).send('Bad Request')
+      })
+  }
+}
