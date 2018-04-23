588583e8bf9d6c00c128ac4668221c9bae0e59af
RePloZ <herve.kay@ynov.com>
2018-04-23 15:46:12 +0200

    :tada: v0 => v1

--- models/Workspace.js
+++ models/Workspace.js
@@ -1,10 +1,8 @@
 let serverConfiguration = require("../config/server");
 let mongoose = require("mongoose");
-
+const autoPopulatePlugin = require('mongoose-autopopulate');
 let Organization = require("./nestedSchema/NestedOrganizationSchema");
 let User = require("./nestedSchema/NestedUserSchema");
-let NestedNode = require("./nestedSchema/NestedNodeSchema");
-let NestedTeam = require("./nestedSchema/NestedTeamSchema");
 let Node = require("./Node");
 
 const NestedAnnotation = require('./nestedSchema/NestedAnnotation');
@@ -12,20 +10,21 @@ let Stripe = new mongoose.Schema({
   name: String
 });
 
+const Team = new mongoose.Schema({
+  orga
+})
+
 let WorkspaceSchema = new mongoose.Schema({
   name: { type: String, require: true },
-  owner: { type: User, require: true },
   description: String,
+  nodeMaster: { type: Schema.Types.ObjectId, ref: 'Node' },
   node_master: { type: NestedNode },
   creation: { type: Date, default: new Date() },
-  users: { type: [User], required: false, default: [] },
-  team: { type: NestedTeam, required: true },
   organization: { type: Organization, required: true }, // /!\ WITHOUT END VARIABLE /!\
-  Annotations: {type: [NestedAnnotation], required: true, default: [] }
+  Annotations: { type: [NestedAnnotation], required: true, default: [] },
 });
 
-// TODO: DELETE ALL users attribute (workspace.users)
-
+WorkspaceSchema.plugin(autoPopulatePlugin);
 /**
  *
  *
