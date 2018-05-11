39e8ec177a5054c1430168460b3c35dd8f5385ed
RePloZ <herve.kay@ynov.com>
2018-05-11 18:20:25 +0200

    corrector

--- utils/corrector/WorkspaceCleaner.js
+++ utils/corrector/WorkspaceCleaner.js
@@ -6,9 +6,21 @@ module.exports = function* () {
   for (let doc = yield cursor.next(); doc !== null; doc = yield cursor.next()) {
     const workspace = doc._id;
     if (workspace.node_master) {
-      doc.nodeMaster = workspace.node_master._id;
+      const nodeMaster = doc.get('node_master._id');
+      doc.nodeMaster = nodeMaster;
       doc.node_master = null;
       yield doc.save();
+    }
+    const users = doc.get('users');
+    const owner = doc.get('owner._id')
+    if(users) {
+      dov.P = [owner];
+      let usersId = users.map(user => user._id);
+      doc.Teammates = usersId.filter(user => {
+        const id = String(user);
+        const filter = String(owner);
+        return id !== filter;
+      });
     }
   }
 };
