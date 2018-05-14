f87eae66c378edaca8cc69895708cb5bb810258b
RePloZ <herve.kay@ynov.com>
2018-05-14 15:32:09 +0200

    :wrench: UserCleaner

--- utils/corrector/UserCleaner.js
+++ utils/corrector/UserCleaner.js
@@ -11,19 +11,16 @@ module.exports = function* () {
       let nestedWorkspace = user.workspaces;
       for (let i = 0; i < nestedWorkspace.length; i++) {
         let workspace = yield workspaceSchema.findById(nestedWorkspace[i]._id);
-        const Organization = user.organizations;
-        console.log(doc._id);
-        const organization = yield organizationSchema.findOne({ 'workspaces._id': doc._id });
-        console.log(organization)
-        const isExisting = Organization.findIndex(({_id}) => {
+        const OrganizationManager = user.get('Organization');
+        const organization = yield organizationSchema.findOne({ 'Workspaces': doc._id });
+        const isExisting = OrganizationManager.findIndex(({_id}) => {
           const id1 = String(_id);
           const id2 = String(organization._id);
           return id1 === id2;
         });
         if(isExisting && isExisting !== -1) {
-          doc.Organization[isExisting].workspaces.push(workspace._id)
+          doc.Organization[isExisting].workspaces.push(workspace._id);
         } else {
-          console.log(organization._id);
            user.Organization.push({
              _id: organization._id,
             workspaces: [workspace._id],
