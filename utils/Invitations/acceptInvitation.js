33c13fe7a9e84bfa73f17f673508662a98b4cab2
RePloZ <herve.kay@ynov.com>
2018-05-24 12:44:54 +0200

    :bug: :card_file_box: Database changement

--- utils/Invitations/acceptInvitation.js
+++ utils/Invitations/acceptInvitation.js
@@ -1,9 +1,55 @@
 const Invitation = require('../../models/Invitation');
-const Organisation = require('../../models/Organization');
-const Workspace = require('../../models/Workspace');
-const _ =  require('lodash');
+const organisationSchema = require('../../models/Organization');
+const workspaceSchema = require('../../models/Workspace');
+const _ = require('lodash');
 const invitationError = { code: 1, message: 'Invalid Invitation' };
 const internalError = { code: 2, message: 'Internal Error' };
+
+async function acceptInvitation(invitationUid, user) {
+  const invitation = await Invitation.findOne({ uid: invitationUid });
+  if (!invitation) throw new Error('[Invitaiton] - Invitation Not Found');
+  const { workspace, organization } = invitation;
+
+  if (organization) {
+    const Organization = await organisationSchema.findById(invitation.organization.id);
+    switch (organization.role) {
+      case 5:
+        await Organization.addAdmin(user._id);
+        break;
+      case 4:
+        await Organization.addMember(user._id);
+        break;
+      default:
+        throw new Error('The Organization has no role');
+        break;
+    }
+  } else throw new Error('You cannot invite people without an organization')
+
+
+  if (workspace) {
+    const Workspace = await workspaceSchema.findById(invitation.Workspace.id);
+    switch (workspace.role) {
+      case 1:
+        await Workspace.addProductManager(user._id);
+        break;
+      case 2:
+        await Workspace.addTeammate(user._id);
+        break;
+      case 3:
+        throw new Error('Cannot use Observer');
+        //Workspace.addObserver(user._id);
+        break;
+      default:
+        throw new Error('The Workspace has no role');
+        break;
+    }
+    await invitation.remove();
+    return `/project/${Workspace._id}`;
+  } else {
+    await invitation.remove();
+    return `/organization/${Organization._id}`;    
+  } 
+}
 
 /**
  * Accept the Invitation
@@ -12,7 +58,7 @@ const internalError = { code: 2, message: 'Internal Error' };
  * @return {Promise}
  */
 function acceptInvitation(invitationUid, user) {
-//            .then(res => updateTeam(res.team, res.user))
+  //            .then(res => updateTeam(res.team, res.user))
   return new Promise((resolve, rej) => {
     let invitation = null;
     Invitation.findOne({ uid: invitationUid })
@@ -40,7 +86,7 @@ function updateUser(inv, user) {
   return new Promise((res, rej) => {
     if (inv === null) rej(invitationError);
 
-    let indiceOrganization = _.findIndex(user.Organization, function({_id}) {
+    let indiceOrganization = _.findIndex(user.Organization, function ({ _id }) {
       const id1 = String(_id);
       const id2 = String(inv.organization.id);
       console.log(id1 === id2);
@@ -49,11 +95,11 @@ function updateUser(inv, user) {
     if (indiceOrganization && indiceOrganization !== -1)
       user.Organization[indiceOrganization].workspaces.push(inv.workspace.id);
     else
-      user.Organization.push({_id: inv.organization.id, workspaces: [inv.workspace.id]});
+      user.Organization.push({ _id: inv.organization.id, workspaces: [inv.workspace.id] });
 
     user.save()
       .then(() => {
-        Workspace.findOne({ _id: inv.workspace.id })
+        workspaceSchema.findOne({ _id: inv.workspace.id })
           .then((w) => {
             console.log('w =', w);
             res({ workspace: w, user });
