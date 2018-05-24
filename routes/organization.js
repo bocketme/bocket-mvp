d2483ba36139fe85a155b29c0e56d9f332b32eff
RePloZ <herve.kay@ynov.com>
2018-05-24 12:39:02 +0200

    :ok_hand: route organization change Info

--- routes/organization.js
+++ routes/organization.js
@@ -6,6 +6,7 @@ const organizationController = require('../controllers/organizationController');
 router.get('/:organizationId', organizationController.index);
 router.get('/:organizationId/members', organizationController.members);
 router.get('/:organizationId/workspaces', organizationController.workspaces);
+router.post('/:organizationId/info', organizationController.changeInformation);
 router.post('/:organizationId/delete', organizationController.deleteOrganization);
 router.post('/:organizationId/workspace', organizationController.createWorkspace)
 module.exports = router;
