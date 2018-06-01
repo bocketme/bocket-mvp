bf0edb59c5520da064cf8c86090ce19c2a2a178c
RePloZ <herve.kay@ynov.com>
2018-06-01 18:06:37 +0200

    :truck: project => workspace

--- routes/workspaces.js
+++ routes/workspaces.js
@@ -3,6 +3,22 @@ const router = Router();
 
 const workspaceController = require('../controllers/workspaceController');
 
+const controller = require('../controllers/Workspace')
+
 router.post('/:workspaceId/information', workspaceController.changeInfo);
+
+router.get('/image', (req, res) => res.status(404).send('Not Found'))
+//Render the user page
+router.get('/:workspaceId', controller.update.changeOption, controller.get.index);
+
+//Create a new Workspace
+router.post('/', controller.post.indexPost)
+router.post('/:workspaceId');
+
+//Update the workspace Information
+router.put('/:workpsaceId/information', controller.update.changeInformation);
+
+//Delete the workspace
+router.delete('/:workspaceId', controller.delete.deleteWorkspace);
 
 module.exports = router;
