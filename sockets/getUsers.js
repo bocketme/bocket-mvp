1be4b4e1fbe7adf9b35e76e3cccffd92d1d266b7
RePloZ <herve.kay@ynov.com>
2018-06-08 10:53:44 +0200

    :wrench: Tchat

--- sockets/getUsers.js
+++ sockets/getUsers.js
@@ -5,13 +5,17 @@ module.exports = (io, socket) => {
   socket.on('[Users] - fetchFromWorkspace', async (withCurrentUser = false) => {
     try {
       const { userMail } = socket.handshake.session;
-      const { users } = await Workspace
+      const workspace = await Workspace
         .findById(socket.handshake.session.currentWorkspace)
-        .populate('ProductManagers')
-        .populate('Teammates')
-        .populate('Observers')
+        .populate({path: 'ProductManagers', select: 'completeName email'})
+        .populate({path: 'Teammates', select: 'completeName email'})
+        .populate({path: 'Observers', select: 'completeName email'})
         .exec();
 
+      if (!workspace)
+        throw new Error('[Users] - Fetch From Workspace');
+
+      const users = workspace.users;
       if (withCurrentUser === false) {
         let tmpId = 0;
         users.sort();
@@ -32,15 +36,8 @@ module.exports = (io, socket) => {
 
   socket.on('[Users] - fetchById', async (id) => {
     try {
-      const { users } = await Workspace
-        .findById(socket.handshake.session.currentWorkspace)
-        .populate('ProductManagers')
-        .populate('Teammates')
-        .populate('Observers')
-        .exec();
-
-      const fetchedUser = users.find(elem => elem._id === id);
-      socket.emit('[Users] - fetchById', fetchedUser);
+      const user = User.findById(id).select('completeName');
+      socket.emit('[Users] - fetchById', user);
     } catch (err) {
       console.error(err);
     }
