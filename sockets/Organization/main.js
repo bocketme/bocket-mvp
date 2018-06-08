52ca9d9dfa15d397e3a17c142b96b573daba3475
RePloZ <herve.kay@ynov.com>
2018-06-08 10:46:47 +0200

    :truck: main => index.js

--- sockets/Organization/main.js
+++
@@ -1,8 +0,0 @@
-const add = require('./add');
-const fetch = require('./fetch');
-const leave = require('./leave');
-const remove = require('./remove');
-
-module.exports = (io, socket) => {
-  add(io, socket);
-}
