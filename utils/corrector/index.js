c6686308d07ed0d277c0e077f0ef2fdfa4421cb0
RePloZ <herve.kay@ynov.com>
2018-06-11 16:49:33 +0200

    :loud_sound: corrector

--- utils/corrector/index.js
+++ utils/corrector/index.js
@@ -3,12 +3,16 @@
 const organizationCleaner = require('./OrganizationCleaner');
 const workspaceCleaner = require('./WorkspaceCleaner');
 const userCleaner = require('./UserCleaner');
+const log = require('../log')
 
-module.exports = function*() {
-  console.log(1)
+module.exports = function* () {
+  log.info('[Correction] - Organization : Started...');
   yield organizationCleaner();
-  console.log(1)
+  log.info('[Correction] - Organization : Finised');
+  log.info('[Correction] - Workspace : Started...');
   yield workspaceCleaner();
-  console.log(1)
+  log.info('[Correction] - Workspace : Finised');
+  log.info('[Correction] - User : Started...');
   yield userCleaner();
+  log.info('[Correction] - User : Finised');
 }
