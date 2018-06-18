93a64d08c9bd50a6ee5a9c7e5d522fe0f2520473
RePloZ <herve.kay@ynov.com>
2018-06-18 16:02:14 +0200

    :wrench: removeUser.js

--- sockets/Workspace/removeUser.js
+++ sockets/Workspace/removeUser.js
@@ -1,17 +1,22 @@
 const workspaceSchema = require('../../models/Workspace');
 const organizationSchema = require('../../models/Organization');
+const userSchema = require('../../models/User');
 const twig = require('twig');
+const log = require('../../utils/log');
 
 module.exports = (io, socket) => {
-  socket.on('[Workspace] - remove', async (workspaceId, userId) => {
+  socket.on('[Workspace] - remove user', async (workspaceId, userId) => {
     try {
       const currentUser = socket.handshake.session.userId
       const workspace = await workspaceSchema.findById(workspaceId);
       const organization = await organizationSchema.findById(workspace.Organization);
 
       const rights = organization.userRights(currentUser);
-      
-      if (organization.isOwner(currentUser) || organization.isAdmin(currentUser) || workspace.isProductManager(currentUser))
+
+      if (organization.isOwner(currentUser) ||
+        organization.isAdmin(currentUser) ||
+        workspace.isProductManager(currentUser) ||
+        currentUser._id.equals(userId))
         await workspace.removeUser(userId);
       else return socket.emit('[Workspace] - remove', 'You have no right for this workspace');
 
@@ -31,9 +36,24 @@ module.exports = (io, socket) => {
         currentUser: { rights }
       }, function (err, html) {
         if (err) {
-          console.error(err)
+          log.error(err)
           return socket.emit('[Workspace] - remove', 'Please recharge the page');
-        } else return socket.emit('[Workspace] - remove', null, html, workspaceId);
+        } return socket.emit('[Workspace] - remove', null, html, workspaceId);
+      });
+
+      const user = await userSchema
+        .findById(userId)
+        .populate('Manager.Organization Manager.Workspaces')
+        .execPopulate();
+
+      twig.renderFile('./socket/OrganizationNonAccess.twig', {
+        title: "You no longer belong to this workspace",
+        Manager: user.Manager
+      }, function (err, html) {
+        if (err)
+          log.error(err);
+        else
+          return io.to(workspace._id).emit('[User] - Not Access', userId, html);
       });
 
     } catch (error) {
