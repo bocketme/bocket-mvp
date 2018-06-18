bab889c1dd43700803d0d4069b68763bd0b989d4
RePloZ <herve.kay@ynov.com>
2018-06-18 16:00:33 +0200

    :sparkles: add user Rights for deletion

--- sockets/NodeManager/delete.js
+++ sockets/NodeManager/delete.js
@@ -1,27 +1,26 @@
 const socketName = '[Node] - Delete';
-const Node = require('../models/Node');
-const User = require('../models/User');
-const Workspace = require('../models/Workspace');
+const Node = require('../../models/Node');
+const User = require('../../models/User');
+const Workspace = require('../../models/Workspace');
 
-const log = require('../utils/log');
+const log = require('../../utils/log');
 
 module.exports = async (io, socket) => {
 
   socket.on(socketName, async function (nodeId) {
     try {
-      const node = Node.findById(nodeId);
+      const node = await Node.findById(nodeId);
       if (!node) throw new Error('Cannot find the node');
-      const workspace = Workspace.findById(node.Workspace);
+      const workspace = await Workspace.findById(node.Workspace);
       if (!workspace) throw new Error('Cannot find the node');
 
       const { session } = socket.handshake
-      const rights = workspace.userRights(session.userId);
+      const rights = workspace.hasRights(session.userId);
 
       if (!rights || rights === 1)
         throw new Error('[Organization] - Cannot delete the node, user has no the necessary rights');
 
       await node.remove();
-
       io.to(session.currentWorkspace).emit(socketName, nodeId);
       io.to(session.currentWorkspace).emit('[Viewer] - Delete', nodeId);
     } catch (err) {
