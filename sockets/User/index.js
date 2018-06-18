e46659b1ee4120bf74018e97b642d37626e3bada
RePloZ <herve.kay@ynov.com>
2018-06-18 15:57:59 +0200

    :sparkle: Join - Reload

--- sockets/User/index.js
+++ sockets/User/index.js
@@ -1,6 +1,11 @@
 const signIn = require('./signIn');
 const getUsersList = require('./getUserList')
+const join = require('./join');
+const reload = require('./reload');
+
 module.exports = (io, socket) => {
   signIn(io, socket);
   getUsersList(io, socket);
+  join(io, socket);
+  reload(io, socket);
 };
---
+++ sockets/User/join.js
@@ -0,0 +1,18 @@
+const organizationSchema = require('../../models/Organization');
+const workspaceSchema = require('../../models/Workspace');
+
+module.exports = (io, socket) => {
+  socket.on("[USER] - join", async function () {
+    const { currentWorkspace, currentOrganization, userId } = socket.handshake.session;
+    console.log(currentWorkspace, currentOrganization)
+    const organization = await organizationSchema.findById(currentOrganization);
+    if (organization) {
+      socket.join(currentOrganization);
+    } else console.log(`Cannot find the organization`)
+    const workspace = await workspaceSchema.findById(currentWorkspace);
+    if (workspace) {
+      socket.join(currentWorkspace);
+    } else console.log(`Cannot find the workspace`)
+    socket.join(userId);
+  });
+}
