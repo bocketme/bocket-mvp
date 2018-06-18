a0a194ec625e0fa9be505fb5382512824343c13f
RePloZ <herve.kay@ynov.com>
2018-06-18 15:30:44 +0200

    :card_file_box: change id property String => ObjectId

--- models/nestedSchema/NestedNodeSchema.js
+++ models/nestedSchema/NestedNodeSchema.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const Node = new mongoose.Schema({
-  _id: { type: String, require: true },
+  _id: { type: mongoose.SchemaTypes.ObjectId, require: true },
   name: { type: String, required: true },
   type: { type: String, required: true },
 });
