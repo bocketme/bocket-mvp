62a8754dfd6c40685adcf55497e03f0494b0d186
RePloZ <herve.kay@ynov.com>
2018-06-20 12:04:09 +0200

    :wrench: Invitation SignIn

--- utils/Invitations/acceptInvitation.js
+++ utils/Invitations/acceptInvitation.js
@@ -2,8 +2,7 @@ const Invitation = require('../../models/Invitation');
 const organisationSchema = require('../../models/Organization');
 const workspaceSchema = require('../../models/Workspace');
 const _ = require('lodash');
-const invitationError = { code: 1, message: 'Invalid Invitation' };
-const internalError = { code: 2, message: 'Internal Error' };
+const log = require('../../utils/log');
 
 async function acceptInvitation(invitationUid, user) {
   const invitation = await Invitation.findOne({ uid: invitationUid });
@@ -17,20 +16,19 @@ async function acceptInvitation(invitationUid, user) {
     result.organizationId = Organization._id;
     switch (organization.role) {
       case 5:
-        await Organization.addAdmin(user._id);
+        await Organization.addAdmin(user._id).catch(err => log.warn('[Organization] - The user already exists. skipping... \n' + err));
         break;
       case 4:
-        await Organization.addMember(user._id);
+        await Organization.addMember(user._id).catch(err => log.warn('[Organization] - The user already exists. skipping...' + err))
         break;
       default:
         throw new Error(`The Organization has no role or the role is incorect, role = ${role}`);
-        break;
     }
   } else throw new Error('You cannot invite people without an organization')
 
   if (workspace) {
     const Workspace = await workspaceSchema.findById(invitation.workspace.id);
-    result.workspaceId = Workspace._id;    
+    result.workspaceId = Workspace._id;
     switch (workspace.role) {
       case 3:
         await Workspace.addProductManager(user._id);
@@ -40,73 +38,21 @@ async function acceptInvitation(invitationUid, user) {
         break;
       case 1:
         throw new Error('Cannot use Observer');
-        //Workspace.addObserver(user._id);
+      /*
+        Workspace.addObserver(user._id);
         break;
+      */
       default:
         throw new Error('The Workspace has no role');
-        break;
     }
     await invitation.remove();
-    result.url = `/project/${Workspace._id}`;
+    result.url = `/workspace/${Workspace._id}`;
     return result;
   } else {
     await invitation.remove();
     result.url = `/organization/${Organization._id}`;
     return result;
   }
-}
-
-function updateUser(inv, user) {
-  return new Promise((res, rej) => {
-    if (inv === null) rej(invitationError);
-
-    let indiceOrganization = _.findIndex(user.Organization, function ({ _id }) {
-      const id1 = String(_id);
-      const id2 = String(inv.organization.id);
-      console.log(id1 === id2);
-      return id1 === id2;
-    });
-    if (indiceOrganization && indiceOrganization !== -1)
-      user.Organization[indiceOrganization].workspaces.push(inv.workspace.id);
-    else
-      user.Organization.push({ _id: inv.organization.id, workspaces: [inv.workspace.id] });
-
-    user.save()
-      .then(() => {
-        workspaceSchema.findOne({ _id: inv.workspace.id })
-          .then((w) => {
-            console.log('w =', w);
-            res({ workspace: w, user });
-          })
-          .catch(err => rej(err));
-      })
-      .catch(err => rej(err));
-  });
-}
-
-function updateWorkspace(workspace, user) {
-  return new Promise((res, rej) => {
-    console.log('workspace = ', workspace);
-    workspace.Teammates.push(user._id);
-    workspace.save()
-      .then(w => res(w))
-      .catch(err => rej(err));
-  });
-}
-/*function updateTeam(team, user) {
-    return new Promise((res, rej) => {
-        console.log("Team = ", team);
-        team.members.push({_id: user._id, completeName: user.completeName, email: user.email});
-        team.save()
-            .then((w) => res(w._id))
-            .catch(err => rej(err));
-    });
-} */
-
-async function updateOwnerOrganization({ _id }, organizationId) {
-  const orga = await Organisation.findById(organizationId);
-  orga.Members.push(_id);
-  orga.save();
 }
 
 module.exports = acceptInvitation;
