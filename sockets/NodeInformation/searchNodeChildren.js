b37c388a3e4dec09d84b8e18267b2d5e8ae2366f
RePloZ <herve.kay@ynov.com>
2018-06-25 14:52:33 +0200

    :notebook: need rework !!!

--- sockets/NodeInformation/searchNodeChildren.js
+++ sockets/NodeInformation/searchNodeChildren.js
@@ -1,28 +1,29 @@
-const Node = require('../../models/Node');
+const nodeSchema = require('../../models/Node');
 let TypeEnum = require('../../enum/NodeTypeEnum');
 let twig = require('twig');
+const log = require('../../utils/log');
 
-module.exports = (socket) => {
-    socket.on("nodeChildren", (nodeId, breadcrumbs, sub_level) => {
-        Node.findById(nodeId)
-            .then((node) => {
-                node.children.forEach(child => {
-                    if(child.title)
-                        child.name = child.title;
-                        child.breadcrumb = breadcrumbs + '/' + child.name;
-                    });
-                sub_level++;
-                twig.renderFile('./views/socket/three_child.twig', {
-                    node: node,
-                    TypeEnum: TypeEnum,
-                    sub_level: sub_level
-                }, (err, html) => {
-                    if (err){
-                        log.error('[Socket] - node Children\n', err);
-                        socket.emit("error", err);
-                    }
-                    else socket.emit("nodeChild", html, nodeId);
-                });
-            });
-    });
+module.exports = (io, socket) => {
+  socket.on("nodeChildren", (nodeId, breadcrumbs, sub_level) => {
+    nodeSchema.findById(nodeId)
+      .then((node) => {
+        node.children.forEach(child => {
+          if (child.title)
+            child.name = child.title;
+          child.breadcrumb = breadcrumbs + '/' + child.name;
+        });
+        sub_level++;
+        twig.renderFile('./views/socket/three_child.twig', {
+          node: node,
+          TypeEnum: TypeEnum,
+          sub_level: sub_level
+        }, (err, html) => {
+          if (err) {
+            log.error('[Socket] - node Children\n', err);
+            socket.emit("error", err);
+          }
+          else socket.emit("nodeChild", html, nodeId);
+        });
+      });
+  });
 };
