0a8f86064a0abd50f644edbe293d34563aad1e93
RePloZ <herve.kay@ynov.com>
2018-07-03 14:09:21 +0200

    :wrench: Workspace Cleaner

--- database/corrector/WorkspaceCleaner.js
+++ database/corrector/WorkspaceCleaner.js
@@ -8,33 +8,27 @@ module.exports = function* () {
     try {
       const workspace = doc.toObject();
 
-      if (workspace.node_master) {
-        doc.nodeMaster = workspace.node_master._id;
-      }
+      if (doc.node_master)
+        doc.nodeMaster = doc.node_master._id;
 
-      if (workspace.organization)
-        workspace.Organization = workspace.organization._id
 
-      if (workspace.users) {
+      if (doc.organization)
+        workspace.Organization = workspace.organization._id;
+
+      if (doc.users) {
         const users = workspace.users;
-        if (workspace.owner) {
-          const owner = workspace.owner[0];
-          if (owner._id) {
-            const organization = yield OrganizationModel.findOne({ "Workspaces": doc._id });
-            doc.ProductManagers = [organization.Owner];
-          }
-        } else {
-          const organization = yield OrganizationModel.findOne({ "Workspaces": doc._id });
-          doc.ProductManagers = [organization.Owner];
-        }
         const usersId = users.map(user => user._id);
         doc.Teammates = usersId.filter(user => {
           const id = String(user);
           const filter = String(doc.ProductManagers[0]._id);
           return id !== filter;
         });
-        yield doc.save();
       }
+
+      const organization = yield OrganizationModel.findOne({ "Workspaces": doc._id });
+      doc.ProductManagers = [organization.Owner];
+
+      yield doc.save();
     } catch (error) { log.error(error) }
   }
 };
